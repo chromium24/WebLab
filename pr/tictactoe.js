47bb43b9e0c38dae9dacfa3c2a050b87e7f3df5b
saffirefox <aeisele@aims.edu>
2014-06-25 13:38:29 -0600

    Tic Tac Toe Code #2. Refined code and removed repetitive if statements.

--- pr/tictactoe.js
+++ pr/tictactoe.js
@@ -5,56 +5,62 @@
 //create a program to determine if there is a winner in Tic Tac Toe. There is no user input. The array is pre-set.
 //In function, need to write conditions to check rows and columns. **Row check= 0,1,2/ 3,4,5/ 6,7,8.** Col. check= 0,3,6/1,4,7/2,5,8**Diagonal check= 0,4,8/ 2,4,6
 
+//6/25/14 Revision
+// I am going to refine this program to remove function duplications. Will use two separate conditions to check for diagonal wins and
+// two nested for loops to check rows and cols.
 
-var grid = ['x', 'x','o', 'x', 'o','x', 'o', 'x','o']; //9 element array containing X's, O's, and empty spaces.
+var grid = ['x', 'x','o', 'o', 'x','x', 'o', 'x','o'];
+
+//9 element array containing X's, O's, and empty spaces. Loops used for checking rows and cols, not diagonals.
 //FUNCTION DEFINITIONS---------------------------------
-var checkGrid= function(grid)
-{
-    var win = false;                                   //default win value at false
-    if(grid[0]===grid[1] && grid[1]===grid[2] && grid[0]===grid[2])                     //check row 1
+var checkGrid;
+checkGrid = function (grid) {
+    var win = false;                                        //default win value at false
+    //BEGIN CHECK DIAGONALS
+    var D = 2;                                               //starting index value for diagonal check
+    if (grid[2 * D - 2 * D] === grid[2 * D] && grid[2 * D] === grid[4 * D]) //diagonal check for grid elements 0,4,8 (top left to bottom right)
     {
-        win = true;
-        coosole.log(win);
+        win = "A diagonal win";                                                                         //if diagonal equality found, then win set to true
+        console.log(win);                                                                   //send confirmation to user
     }
-    else if(grid[3]===grid[4] && grid[4]===grid[5] && grid[3]===grid[5])                     //check row 2
+    if (grid[D] === grid[2 * D] && grid[2 * D] === grid[3 * D] && grid[D] === grid[3 * D])   //diagonal check for grid elements 2,4,6 (top right to bottom left)
     {
-        win = true;
-        console.log(win);
+        win = "A diagonal win";                                                                         //if diagonal equality equality found, then win set to true
+        console.log(win);                                                                   //send confirmation to user
     }
-    else if(grid[6]===grid[7] && grid[7]===grid[8] && grid[6]===grid[8])                     //check row 3
+    else {
+        win = false;                                                                        //redundant, perhaps?
+    }
+
+    //BEGIN CHECK ROWS AND COLS
+    for (var R = 0; R < 7; R + 3)                              //Setting boundaries for Rows where there are 3 rows and will loop through 3 times.
     {
-        win = true;
-        console.log(win);
-    }
-    else if(grid[0]===grid[3] && grid[3]===grid[6] && grid[0]===grid[6])                //check col 1
-    {
-        win = true;
-        console.log(win);
-    }
-    else if(grid[1]===grid[4] && grid[4]===grid[7] && grid[1]===grid[7])                //check col 2
-    {
-        win = true;
-        console.log(win);
-    }
-    else if(grid[2]===grid[5] && grid[5]===grid[8] && grid[2]===grid[8])                //check col 3
-    {
-        win = true;
-        console.log(win);
-    }
-    else if(grid[0]===grid[4] && grid[4]===grid[8] && grid[0]===grid[8])                //check diagonal top left to bottom right
-    {
-        win = true;
-        console.log(win);
-    }
-    else if(grid[2]===grid[4] && grid[4]===grid[6] && grid[2]===grid[6])                //check diagonal bottom left to top right
-    {
-        win = true;
-        console.log(win);
-    }
-    else                                                //no matches are made thus no wins at this point
-    {
-        return false;
-    }
-};//end function checkGrid
+        for (var C = 0; C < 3; C++)                         //Setting boundaries for Columns where there are 3 columns and will loop through 3 times.
+        {
+            if (grid[C] === grid[C + 3] && grid[C + 3] === grid[C + 6] && grid[C] === grid[C + 6]) {
+                win = "A column win";                                                                         //if Row equality found, then win set to true
+                console.log(win);                                                                   //send confirmation to user
+            }
+            else {
+                win = false;
+            }
+        }//end for loop check columns
+
+        if (grid[R] === grid[R + 1] && grid[R + 1] === grid[R + 2] && grid[R] === grid[R + 2])                //Initial value R=0; check Row elements 0,1,2 for equality
+
+        {
+            win = "A row win";                                                                         //if Column equality found, then win set to true
+            console.log(win);                                                                   //send confirmation to user
+        }
+        else {
+            win = false;
+        }
+    }//end for loop check rows
+/*
+    if (win === false) {
+        win = "No one won!";
+    }*/
+};//END FUNCTION DEFINITION checkGrid
+
 
 checkGrid(grid);                                        //call function to check the grid for any matches
